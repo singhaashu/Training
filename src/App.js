26cda3d9c19753b004ebccec5ee653418706b7fc
aashish <aashishsingh181197@gmail.com>
2022-01-21 16:00:39 +0530

    hello every budy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import First from './first.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+
+      <First/>
+     <h1 className='ee'>Welcome to the world of react js</h1>
+     <p className='ss'>Hello EveryOne</p>
+     <h3>My Favourite React Js</h3>
+   
+   <a href="http://www.flipkart.com" target="_blank" title='flipkart'>flipkart</a>
+   {/* <ul type ='circle'>
+  <li>aashish</li>
+  <li>aashish</li>
+  <li>aashish</li>
+  <li>aashish</li>
+  
+</ul> */}
+{/* 
+<dl>
+  <dt>COFFEE</dt>
+  <dd>black coffee</dd>
+  <dt>Milk</dt>
+  <dd>white color</dd>
+</dl> */}
+
     </div>
   );
 }
 
+
 export default App;
+
+//id as a selector
+//class as
+// element
+//list
+//order list
+// by default-number
+//urorder list
+// bullets
+//defination list
+// it consist of defination term (dt)
+// defination description (dd)
+//
+//Inheritence
+//constructors
+//Interface
+//constructors
+//spread operator
+//Array destruturing
+//menu drop down
+//div partitions
+//React App-Table,hyperlink
+//arrow function
+
+
+
+
---
+++ src/first.js
@@ -0,0 +1,76 @@
+export default function First(){
+return(
+    <div>
+        <table className='tab' border={2} align="center">
+     <tr>
+       <th colSpan="5">User Details</th>
+     </tr>
+     <tr>
+       <th>Name</th>
+       <th>Profile</th>
+       <th>Email</th>
+       <th>Contact</th>
+       <th>Address</th>
+       <th>Salary</th>
+     </tr>
+     <tr>
+       <td>Aashish</td>
+     <td>Mern Stack </td>
+     <td>aashish.singh@trangile.com</td>
+     <td>9205117337</td>
+     <td>Ballabgarh</td>
+     <td>15000</td>
+     </tr>
+     <tr>
+       <td>Vikas</td>
+     <td>Mern Stack </td>
+     <td>vikas.singh@trangile.com</td>
+     <td>0963852741</td>
+     <td>Delhi</td>
+     <td>15000</td>
+     </tr>
+     <tr>
+     <td>Vishal</td>
+     <td>accountant</td>
+     <td>vishal.singh@trangile.com</td>
+     <td>0963852741</td>
+     <td>Rohtak</td>
+     <td>25000</td>
+     </tr>
+     <tr>
+     <td>Shubham Bindal</td>
+     <td>Mern Stack</td>
+     <td>bindal.singh@trangile.com</td>
+     <td>9599151547</td>
+     <td>Faridabad</td>
+     <td>35000</td>
+     </tr>
+     <tr>
+     <td>Prince</td>
+     <td>Tour and Travelling</td>
+     <td>garg.singh@trangile.com</td>
+     <td>8447969610</td>
+     <td>Faridabad</td>
+     <td>32000</td>
+     </tr>
+     <tr>
+     <td>Shivank</td>
+     <td>Hr Management</td>
+     <td>shivak.singh@trangile.com</td>
+     <td>9599940086</td>
+     <td>Jaipur</td>
+     <td>40000</td>
+     </tr>
+     <tr>
+     <td>Bharat</td>
+     <td>Soft Developer</td>
+     <td>Bharat.singh@trangile.com</td>
+     <td>8447064958</td>
+     <td>delhi</td>
+     <td>30000</td>
+     </tr>
+   </table>
+    </div>
+)
+
+}
